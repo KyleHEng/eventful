1e4b43184c3c41978a048bf549c0e2cd3cde260a
KyleHEng <kylehussaineng@gmail.com>
2025-10-19 14:33:39 +0100

    "Add content to hero page"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,43 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className=" py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Planning and Booking Events Has Never Been Easier!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Bring your events to life with precision and style.
+            </p>
+            <Button
+              size="lg"
+              asChild
+              className="button bg-[mediumpurple] text-white px-4 py-2 rounded w-full  "
+            >
+              <Link href="#events">See Events</Link>
+            </Button>
+          </div>
+          <Image
+            src="/logo.svg"
+            alt="image for hero section"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          ></Image>
+        </div>
+      </section>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">Give it a try!</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">Search</div>
+      </section>
+    </>
+  );
 }
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-5 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="smiley face logo"
+            width={38}
+            height={38}
+          ></Image>
+        </Link>
+        <p>2025 EVENTful. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
