4ae797f6610ad88060d8de84889260adaa19dff0
KyleHEng <kylehussaineng@gmail.com>
2025-10-19 11:09:07 +0100

    "Started Header component"

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/logo.svg"
+            alt="Smiley face for logo"
+            width={38}
+            height={38}
+          ></Image>
+        </Link>
+        <div className="flex w-32 justify-end gap-3"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
