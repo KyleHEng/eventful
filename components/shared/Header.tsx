022bac32cac226d24e8e6f69832bdb91e6107153
KyleHEng <kylehussaineng@gmail.com>
2025-10-19 12:43:06 +0100

    "Added navitems"

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { MobileNav } from "./MobileNav";
+import { NavItems } from "./NavItems";
 
 const Header = () => {
   return (
@@ -16,7 +17,9 @@ const Header = () => {
             height={38}
           ></Image>
         </Link>
-
+        <nav className="sm:flex-between  w-full max-w-xs">
+          <NavItems></NavItems>
+        </nav>
         <SignedOut>
           <SignInButton>
             <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
